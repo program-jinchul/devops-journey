56e3f29e78c2867488ba73de9535de87b49f6913
jinchul <pjc5767830@gmail.com>
2026-07-07 15:15:13 +0900

    feat: add EC2 start/stop automation with boto3

--- python/aws_test.py
+++ python/aws_test.py
@@ -4,6 +4,16 @@ ec2 = boto3.client('ec2', region_name='ap-northeast-2')
 
 response = ec2.describe_instances()
 
+
+
+action = input('start or stop:')
+if action == 'start':
+    print('EC2를 실행합니다.')
+    start = ec2.start_instances(InstanceIds=['i-0e36eada750cf6cab'])
+else:
+    print('EC2를 중지합니다.')
+    stop = ec2.stop_instances(InstanceIds=['i-0e36eada750cf6cab'])
+
 for reservation in response['Reservations']:
     for instance in reservation['Instances']:
         print(f"인스턴스 ID: {instance['InstanceId']}")
